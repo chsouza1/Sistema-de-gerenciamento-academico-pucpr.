59bf3dfd522d425aba47341b12e49102a35a5472
Carlos Henrique <127338261+chsouza1@users.noreply.github.com>
2023-08-17 21:19:53 -0300

    Add files via upload

---
+++ Sistema de Gerenciamento Acadêmico.py
@@ -0,0 +1,27 @@
+print("Seja Bem Vindo(a), ao sistema de gestão de dados acadêmicos da Puc Paraná!")
+
+print("########## Menu do usuario ##########")
+
+print("1 - Estudante;")
+print("2 - Disciplina;")
+print("3 - Professor;")
+print("4 - Turma;")
+print("5 - matricula")
+
+tecla_digitada_menu_principal = int(input("Escolha uma das opções do menu acima: "))
+
+print(f"Ok, voce escolheu a opção {tecla_digitada_menu_principal} do menu acima.")
+
+print("Agora, o que voce deseja fazer com sua opção? ")
+
+print("1 - incluir;")
+print("2 - listar;")
+print("3 - excluir;")
+print("4 - alterar;")
+
+tecla_digitada_menu_secundario = int(input("Escolha uma das opçoes acima: "))
+
+print(f"Você escolheu a alternativa {tecla_digitada_menu_secundario} das opções acima.")
+
+#proxima semana#
+
